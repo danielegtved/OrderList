d55aa0d4a369685433de11a6ceff6261b22637c5
danielegtved <43374944+danielegtved@users.noreply.github.com>
2018-11-14 20:35:54 +0100

    Initial commit

---
+++ vandMændList.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using OrderList.Annotations;
+
+namespace OrderList
+{
+    class vandMændList : INotifyPropertyChanged
+    {
+        private string _tentacleLength;
+        private string _name;
+        private string _deepThoughtsFromTheDeepOcean;
+        public vandMændList(string tentacleLength, string name, string deepThoughtsFromTheDeepOcean)
+        {
+            this._tentacleLength = tentacleLength;
+            this._name = name;
+            this._deepThoughtsFromTheDeepOcean = deepThoughtsFromTheDeepOcean;
+        }
+
+        public string TentacleLength
+        {
+            get { return _tentacleLength; }
+            set { _tentacleLength = value; }
+        }
+
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        public string DeepThoughtsFromTheDeepOcean
+        {
+            get { return _deepThoughtsFromTheDeepOcean; }
+            set { _deepThoughtsFromTheDeepOcean = value; }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ viewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using OrderList.Annotations;
+
+namespace OrderList
+{
+    class viewModel : INotifyPropertyChanged
+    {
+        private ObservableCollection<vandMændList> _vandMændLists = new ObservableCollection<vandMændList>();
+
+        public viewModel()
+        {
+            _vandMændLists.Add(new vandMændList("27", "Benny", "fisk"));
+            _vandMændLists.Add(new vandMændList("28", "Kaj", "light"));
+            _vandMændLists.Add(new vandMændList("too long", "Finn", "darkness"));
+        }
+
+
+  
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<vandMændList> VandMændLists
+        {
+            get { return _vandMændLists; }
+            set { _vandMændLists = value; }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
